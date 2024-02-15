3797b0738ebd4025a5048c7c03528660b6bbc784
Ümit Aktaş <152315417+aktasumitt@users.noreply.github.com>
2024-02-15 14:52:21 +0300

    Update

--- visualization.py
+++ visualization.py
@@ -12,18 +12,7 @@ def visualization_dataset(dataset):
         plt.yticks([])
     
     plt.show()
-    
-    
 
-def Visualization_test_predict(test_dataloader,prediction_test):
-    
-    for batch_test,(img,label) in enumerate(test_dataloader,0):
-        
-        plt.subplot(batch_test/4,4,batch_test+1)
-        plt.imshow(torch.transpose(torch.transpose(img[0],0,2),0,1))
-        plt.xlabel(f"predict: {prediction_test[batch_test][0]},real: {label[0]} ")
-    
-    plt.show()
     
     
 
